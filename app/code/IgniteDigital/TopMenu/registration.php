fb815db872e71b04c413a258ecc6b828bdf77070
BrianShivraj <brian@ignitedigital.com>
2020-06-26 14:30:13 +0000

    Theme

---
+++ app/code/IgniteDigital/TopMenu/registration.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Ignite Digital , Ltd.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GiaPhuGroup.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.ignitedigital.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    IgniteDigital
+ * @package     IgniteDigital_TopMenu
+ * @copyright   Copyright (c) 2019-2020 Ignite Digital , Ltd. All rights reserved. (http://www.ignitedigital.com/)
+ * @license     https://www.ignitedigital.com/LICENSE.txt
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'IgniteDigital_TopMenu',
+    __DIR__
+);
